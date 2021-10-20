c3240b334c15d23f653d909f0996e23ae402103c
coderaditya01 <87324283+coderaditya01@users.noreply.github.com>
2021-10-20 15:24:49 +0530

    up

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express()
 
 //port (as described above) and host are both wrong
 const host = 'localhost';
-const port = 3000;
 //use alternate localhost and the port Heroku assigns to $PORT
 const port = process.env.PORT || 3000;
 
